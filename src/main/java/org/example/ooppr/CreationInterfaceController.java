ca649268221bf16c950b3da3d2e46254b608f0c7
JevGolo <jev.golosov@gmail.com>
2025-04-06 16:23:10 +0300

    Added Error alerts and resolution fields validation

--- src/main/java/org/example/ooppr/CreationInterfaceController.java
+++ src/main/java/org/example/ooppr/CreationInterfaceController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -22,21 +23,62 @@ public class CreationInterfaceController {
     @FXML
     public TextField YResolutionHolder;
 
+    /**
+     * The method switches the scene to Product. Checks the sizes of the passed canvas XResolutionHolder and YResolutionHolder.
+     */
     public void SwitchToPaintPanel(ActionEvent event) throws IOException {
-        // Loading scene
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("product.fxml"));
-        root = loader.load();
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
 
-        // Getting canvas resolution and setting it
-        int XResolution = Integer.parseInt(XResolutionHolder.getText());
-        int YResolution = Integer.parseInt(YResolutionHolder.getText());
+        try {
+            // Getting canvas resolution
+            int XResolution = Integer.parseInt(XResolutionHolder.getText());
+            int YResolution = Integer.parseInt(YResolutionHolder.getText());
 
-        ProductController productController = loader.getController();
-        productController.setCanvasSize(XResolution, YResolution);
+            if ( resolutionIsValid(XResolution, YResolution) ) {
+                // Loading scene
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("product.fxml"));
+                root = loader.load();
+                stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
 
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+                // getting controller, canvas sizing
+                ProductController productController = loader.getController();
+                productController.setCanvasSize(XResolution, YResolution);
+
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } else {
+                // if negative value or 0 then alerts error
+                showAlert("Error", "Resolution Error", "Please enter a valid resolution (positive integer)!)");
+            }
+        } catch (NumberFormatException e) {
+            // If not-numerical value then alerts error
+            showAlert("Error", "Invalid input", "Please input numerical values");
+        }
+
     }
+
+    /**
+     * The method checks the validity of the canvas size
+     * @param xResolution canvas x resolution (width)
+     * @param yResolution canvas y resolution (height)
+     * @return true if valid, false if not
+     */
+    private boolean resolutionIsValid(int xResolution, int yResolution) {
+        return xResolution > 0 && yResolution > 0;
+    }
+
+    /**
+     * The method shows alerts
+     * @param title alert title text
+     * @param header alert header text
+     * @param content alert content text
+     */
+    private void showAlert(String title, String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle( title );
+        alert.setHeaderText( header );
+        alert.setContentText( content );
+        alert.showAndWait();
+    }
+
 }
