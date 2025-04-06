1dd61542d0d6b5ba0e2c01da73ee49be4757f308
JevGolo <jev.golosov@gmail.com>
2025-04-06 15:58:46 +0300

    Added dynamic canvas size changing

--- src/main/java/org/example/ooppr/CreationInterfaceController.java
+++ src/main/java/org/example/ooppr/CreationInterfaceController.java
@@ -1,10 +1,12 @@
 package org.example.ooppr;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,9 +16,24 @@ public class CreationInterfaceController {
     private Stage stage;
     private Parent root;
 
+    @FXML
+    public TextField XResolutionHolder;
+
+    @FXML
+    public TextField YResolutionHolder;
+
     public void SwitchToPaintPanel(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("product.fxml"));
+        // Loading scene
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("product.fxml"));
+        root = loader.load();
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        // Getting canvas resolution and setting it
+        int XResolution = Integer.parseInt(XResolutionHolder.getText());
+        int YResolution = Integer.parseInt(YResolutionHolder.getText());
+
+        ProductController productController = loader.getController();
+        productController.setCanvasSize(XResolution, YResolution);
 
         scene = new Scene(root);
         stage.setScene(scene);
--- src/main/java/org/example/ooppr/ProductController.java
+++ src/main/java/org/example/ooppr/ProductController.java
@@ -1,4 +1,27 @@
 package org.example.ooppr;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class ProductController {
+
+    // Getting elements
+    @FXML
+    public Canvas paintingZone;
+
+    public void setCanvasSize( int XResolution, int YResolution ) {
+        paintingZone.setWidth(XResolution);
+        paintingZone.setHeight(YResolution);
+        Platform.runLater(() -> fillCanvasColor());
+    }
+
+    private void fillCanvasColor() {
+        GraphicsContext gc = paintingZone.getGraphicsContext2D();
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, paintingZone.getWidth(), paintingZone.getHeight());
+    }
+
 }
