def8f582307181fa48f6c5064025634f0e7a149d
JevGolo <jev.golosov@gmail.com>
2025-04-18 11:44:39 +0300

    Added ServerOptions join button functionality (switching to CreationIntefrace)

---
+++ src/main/java/org/example/ooppr/ServerOptionsController.java
@@ -0,0 +1,43 @@
+package org.example.ooppr;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ServerOptionsController {
+
+    private Scene scene;
+    private Stage stage;
+    private Parent root;
+
+    @FXML
+    public Button ButtonJoinServer;
+
+    @FXML
+    public Button ButtonHostServer;
+
+    /**
+     * Switching to CreationInterface
+     */
+    public void SwitchToJoin(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("CreationInterfaceV2.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        // getting controller, canvas sizing
+        CreationInterfaceController creationInterfaceController = loader.getController();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
