65b067f656a7f5cc0e4b8607137d8074c5ddb671
krosta12 <kostailusenko911@gmail.com>
2025-04-22 14:45:47 +0300

    idk

--- src/main/java/org/example/ooppr/ServerOptionsController.java
+++ src/main/java/org/example/ooppr/ServerOptionsController.java
@@ -1,11 +1,5 @@
 package org.example.ooppr;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class ServerOptionsController {
     public void host(javafx.event.ActionEvent event) {
         System.out.println("host");
