0ce443daec2aefeb5a1693f1bb6cc15eb64f6dbd
krosta12 <kostailusenko911@gmail.com>
2025-04-22 14:43:15 +0300

    server option

---
+++ src/main/java/org/example/ooppr/ServerOptionsController.java
@@ -0,0 +1,18 @@
+package org.example.ooppr;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ServerOptionsController {
+    public void host(javafx.event.ActionEvent event) {
+        System.out.println("host");
+    }
+
+
+    public void join(){
+        System.out.println("join");
+    }
+}
