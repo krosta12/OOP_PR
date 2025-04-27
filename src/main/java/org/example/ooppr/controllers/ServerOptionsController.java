75c198a07331d5cf06d0de90851f99a3f098a346
krosta12 <kostailusenko911@gmail.com>
2025-04-27 14:49:36 +0300

    controller hotfix

---
+++ src/main/java/org/example/ooppr/controllers/ServerOptionsController.java
@@ -0,0 +1,48 @@
+package org.example.ooppr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ServerOptionsController {
+    // Метод, вызываемый при нажатии на кнопку "Host" + УБРАТЬ active флагв из IP инпута, вероятно переменить на порт
+    public void host(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/ooppr/CreationInterfaceV2.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Получаем контроллер
+            CreationInterfaceController controller = fxmlLoader.getController();
+            controller.setConnectionType("host"); // Передаем "host"
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void join(ActionEvent event) { // меняем join() чтобы он тоже принимал ActionEvent
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/ooppr/CreationInterfaceV2.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Получаем контроллер
+            CreationInterfaceController controller = fxmlLoader.getController();
+            controller.setConnectionType("join"); // Передаем "join"
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
