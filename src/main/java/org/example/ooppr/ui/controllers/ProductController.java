518b928eed96df06039b0a3c08822a5597f1946e
JevGolo <jev.golosov@gmail.com>
2025-05-16 23:32:26 +0300

    Vot now examples really deletes

--- src/main/java/org/example/ooppr/ui/controllers/ProductController.java
+++ src/main/java/org/example/ooppr/ui/controllers/ProductController.java
@@ -86,24 +86,6 @@ public class ProductController implements Initializable {
 
         // Connections info
         ConnectionsManager connectionsManager = new ConnectionsManager( connectionsWrapper );
-        connectionsManager.addUser( new User( "Bob", User.Role.CREATOR, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Anna", User.Role.ADMIN, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Oleg", User.Role.VIEW_ONLY, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Andrew", User.Role.CREATOR, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Alina", User.Role.ADMIN, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Aleksander", User.Role.EDIT, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Anastassia", User.Role.VIEW_ONLY, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Martin", User.Role.EDIT, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Katrin", User.Role.ADMIN, LocalDateTime.now() ) );
-
-        connectionsManager.addUser( new User( "Anna", User.Role.ADMIN, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Oleg", User.Role.VIEW_ONLY, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Andrew", User.Role.CREATOR, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Alina", User.Role.ADMIN, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Aleksander", User.Role.EDIT, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Anastassia", User.Role.VIEW_ONLY, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Martin", User.Role.EDIT, LocalDateTime.now() ) );
-        connectionsManager.addUser( new User( "Katrin", User.Role.ADMIN, LocalDateTime.now() ) );
     }
 
 
